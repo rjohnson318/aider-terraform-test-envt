c86f2df2bb8960585daf9d8959db94af31bc06bd
rjohnson318 <ralphael.johnson@gmail.com>
2023-07-04 16:34:26 -0500

    aider: Added resource group "zone-b-spoke1-rg-02" to the "zone-b" spoke1.
    # Context:
    USER: zone-b spoke1 needs another rg called "zone-b-spoke1-rg-02"
    ASSISTANT: To add another resource group "zone-b-spoke1-rg-02" to the "zone-b" spoke1, we need to modify the `main.tf` file in the `generated2/envs/zone-b/spoke1` directory.
    
    Here's the change:
    
    ```hcl
    generated2/envs/zone-b/spoke1/main.tf
    <<<<<<< ORIGINAL
    =======
    resource "azurerm_resource_group" "rg_02" {
      name     = "zone-b-spoke1-rg-02"
      location = var.location
    }
    >>>>>>> UPDATED
    ```
    
    This will create a new resource group with the name "zone-b-spoke1-rg-02" in the location specified by the `location` variable. Please ensure that the `location` variable is defined in your `vars.tf` file. If it's not, you'll need to add it.

--- generated2/envs/zone-b/spoke1/main.tf
+++ generated2/envs/zone-b/spoke1/main.tf
@@ -44,4 +44,7 @@ module "virtual_machine" {
   subnet_id           = module.subnet.subnet_id
   nsg_id              = module.network_security_group.nsg_id
   asg_id              = module.application_security_group.asg_id
+}resource "azurerm_resource_group" "rg_02" {
+  name     = "zone-b-spoke1-rg-02"
+  location = var.location
 }
